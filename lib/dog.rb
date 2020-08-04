491bd19b9b747b2e6e4f8f699e94a60d7f053a45
Austin Cummings <aac729@live.com>
2020-08-04 18:10:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,1 @@
 # Your code goes here!
-class Dog 
-  def name 
-    puts "Its name is #{name}"
-  end 
-  def bark
-    puts "Woof!"
-  end 
-end 
-
-fido = Dog.new 
-fido.name = ("fido")
-bark = Dog.new 
-bark.speak = ("bark")
